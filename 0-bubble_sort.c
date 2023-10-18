9f451cde2fc1e28bfe82a71aa563dfcaa06edd42
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-18 14:45:19 -0700

    Add a function that applies bubble sort on an array

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using the bubble sort
+ * algorithm, which goes through an array n times, comparing
+ * each two adjacent elements and sorts them
+ * @array: array to be sorted
+ * @size: size of @array
+ *
+ * Return: no return value
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
