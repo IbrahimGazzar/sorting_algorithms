2427cd2cb73e5c0ea9e9378b390c3a8e7aaacc84
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-20 04:56:08 -0700

    Add an escape condition for task 0 and task 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,6 +15,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (size <= 1)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,6 +23,8 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_ind;
 	int temp, minim;
 
+	if (size <= 1)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		minim = array[i];
